8c3baa8363908833c1b2838d3b15e0ac27546af6
pkong-ds <pkong.work@gmail.com>
2026-09-02 14:01:02 +0800

    Fill v0.3.0 checksums in the tap formula and scoop manifest

--- packaging/homebrew/Formula/pokemon-info.rb
+++ packaging/homebrew/Formula/pokemon-info.rb
@@ -7,11 +7,11 @@ class PokemonInfo < Formula
   on_macos do
     on_arm do
       url "https://github.com/pkong-ds/pokemon-info/releases/download/v0.3.0/pokemon-info-0.3.0-darwin-arm64.tar.gz"
-      sha256 "REPLACE_WITH_SHA256_OF_pokemon-info-0.3.0-darwin-arm64.tar.gz"
+      sha256 "bebd81aca711b1a550c6f05e51343fb5f2310da9a74d4b3eb08b2eacce2201f5"
     end
     on_intel do
       url "https://github.com/pkong-ds/pokemon-info/releases/download/v0.3.0/pokemon-info-0.3.0-darwin-amd64.tar.gz"
-      sha256 "REPLACE_WITH_SHA256_OF_pokemon-info-0.3.0-darwin-amd64.tar.gz"
+      sha256 "2253b3fe619571252895b764e4456d2f33d4c889b1128bdcf3c1f4546329a906"
     end
   end
 
